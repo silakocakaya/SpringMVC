3fbcb402c820513ab2b645ad6965721ae55583e2
silakocakaya <67272114+silakocakaya@users.noreply.github.com>
2022-08-12 14:41:34 +0300

    form2

--- SpringMVC/src/main/java/com/spring/mvc/HelloWorldController.java
+++ SpringMVC/src/main/java/com/spring/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.spring.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +17,24 @@ public class HelloWorldController {
 	@RequestMapping("/processForm")
 	public String processForm() {
 		return "helloworld";
+	}
+	
+	@RequestMapping("/showForm2")
+	public String showForm2() {
+		return "helloworld-form2";
+	}
+	
+	@RequestMapping("/makeUpperCase")
+	public String makeUpperCase(HttpServletRequest httpServletRequest, Model model) {
+		
+		String theName = httpServletRequest.getParameter("studentName");
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Yo! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld2";
 	}
 }
--- SpringMVC/src/main/webapp/WEB-INF/view/main-menu.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/view/main-menu.jsp
@@ -1,5 +1,15 @@
+<!DOCTYPE>
 <html>
+
 <body>
-<h2>First Spring MVC</h2>
+
+	<h2>Spring MVC Demo - Home Page</h2>
+
+	<hr>
+
+	<a href="showForm">Hello World form</a>
+	<br><br>
+	<a href="showForm2">Hello World form2</a>
 </body>
+
 </html>
